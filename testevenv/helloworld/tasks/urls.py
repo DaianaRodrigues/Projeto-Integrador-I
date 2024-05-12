4212375cf58e9f7ea9bdffd120c3296395a1c752
Daiana Rodrigues <daiana-rodrigues-2007@hotmail.com>
2024-05-12 16:32:35 -0300

    Ajustes nas configurações

--- testevenv/helloworld/tasks/urls.py
+++ testevenv/helloworld/tasks/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.taskList, name='task-list'),
     path('base/', views.pagina_prin, name='principal'),
-    path('login/', views.user_login, name='login'),
+    path('cadastro_sucesso/', views.pagina_sucesso, name='cadastro_sucesso'),
     path('agendamento/', views.agendamento_view, name='agendamento'),
-    path('cadastro/', views.cadastro_view, name='cadastro')
+    path('cadastro/', views.cadastro_view, name='cadastro'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- testevenv/helloworld/tasks/views.py
+++ testevenv/helloworld/tasks/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 
+from .models import Usuario
+
 def taskList(request):
     return render(request, 'tasks/list.html')
 
@@ -10,6 +12,22 @@ def pagina_prin(request):
     return render(request, 'base.html')
 
 def agendamento_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        # Verifica se o usuário existe no banco de dados
+        try:
+            usuario = Usuario.objects.get(username=username)
+            if usuario.password != password:
+                messages.error(request, 'Senha incorreta')
+                return render(request, 'tasks/list.html')
+        except Usuario.DoesNotExist:
+            messages.error(request, 'Usuário inválido')
+            return render(request, 'tasks/list.html')
+
+        # Se o usuário existe e a senha está correta, faça o que for necessário (como redirecionar para outra página)
+        # ...
     return render(request, 'tasks/agendamento.html')
 
 def cadastro_view(request):
@@ -30,36 +48,7 @@ def cadastro_view(request):
 
     return render(request, 'tasks/cadastro.html')
 
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('agendamento')  # Substitua 'home' pelo nome da sua URL de destino pós-login
-        else:
-            # Retorne uma mensagem de erro se a autenticação falhar
-            messages.error(request, 'Nome de usuário ou senha inválidos')
-            return render(request, 'base.html')
-    return render(request, 'base.html')
-
-##def cadastro_view(request):
-    if request.method == 'POST':
-        # Capturar os dados do formulário
-        nome = request.POST.get('nome')
-        telefone = request.POST.get('telefone')
-        email = request.POST.get('email')
-        senha = request.POST.get('senha')
-
-        # Criar um novo usuário no banco de dados
-        user = User.objects.create_user(username=email, email=email, password=senha)
-        user.first_name = nome
-        user.save()
-
-        # Redirecionar para a página de sucesso ou outra página desejada
-        return redirect('sucesso')
-
-    return render(request, 'cadastro.html')
+def pagina_sucesso(request):
+    return render(request, 'tasks/cadastro_sucesso.html')
 
 
